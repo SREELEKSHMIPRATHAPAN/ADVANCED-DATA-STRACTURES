4f740efaf76080b3df08c8b661e591dab8a87834
SREELEKSHMIPRATHAPAN <75786625+SREELEKSHMIPRATHAPAN@users.noreply.github.com>
2021-03-12 09:37:23 +0530

    Add files via upload

---
+++ ds/Simple c/multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  double a,b;
+  printf("enter two numbers:");
+  scanf("%lf %lf",&a,&b);
+  
+  printf("the result is:%lf", a*b);
+  return 0;
+ }
